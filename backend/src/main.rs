e45160716fb6e1b2f221e37651267f96a8ce51e4
Boukari Yameogo <yameogo.boukari@gmail.com>
2025-03-04 21:38:06 -0500

    fix pipeline

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,6 +1,7 @@
 use std::net::SocketAddr;
 use axum::Router;
 use clap::Parser;
+use tracing_subscriber::fmt::format::FmtSpan;
 use crate::infra::cli::Args;
 use crate::infra::config::Config;
 use crate::infra::dependencies::AppState;
@@ -15,14 +16,19 @@ mod prelude;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    tracing_subscriber::fmt::init();
-
     let args = Args::parse();
+    let config = Config::new(args.config_path.as_str(),args.secret_path.as_str());
 
-    let config = Config::new(
-        args.config_path.as_str(),
-         args.secret_path.as_str()
-        );
+    tracing_subscriber::fmt()
+        .with_env_filter(&config.app().log_level)
+        .with_span_events(FmtSpan::FULL)
+        .with_file(true)
+        .with_target(true)
+        .with_line_number(true)
+        .with_thread_ids(true)
+        .init();
+
+
     let app_state = AppState::new(config.clone());
 
     let router = Router::new()
